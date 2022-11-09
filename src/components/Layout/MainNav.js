e93ec45818027e7bdc00097a4b93a7ab02df58a5
nickyczy@gmail.com <nickycodezy@gmail.com>
2022-11-09 19:44:36 +1100

    added nav bar

--- src/components/Layout/MainNav.js
+++ src/components/Layout/MainNav.js
@@ -1,19 +1,35 @@
-import React from "react";
+import { Spin as Hamburger } from "hamburger-react";
+import React, { useState } from "react";
 import classes from "./MainNav.module.css";
 
 const Nav = () => {
+	const [clicked, setClicked] = useState(false);
+
+	const iconClickedHandler = () => {
+		setClicked(!clicked);
+	};
+
+	const multipleClasses = classes.navbar + " " + classes.active;
+
 	return (
-		<header className={classes.header}>
+		<header>
 			<div className={classes.logo}>Logo</div>
-			<nav className={classes.nav}>
-				<ul>
+			<nav>
+				<ul
+					id={classes.navbar}
+					className={clicked ? multipleClasses : classes.navbar}
+				>
+					<li className={classes.active}>Home</li>
 					<li>Recipes</li>
 					<li>Contact</li>
 					<li>Products</li>
 					<li>Sign in</li>
+					<button>Logout</button>
 				</ul>
-				<button>Logout</button>
 			</nav>
+			<div className={classes.mobile}>
+				<Hamburger toggled={clicked} toggle={setClicked} size={38} />
+			</div>
 		</header>
 	);
 };
